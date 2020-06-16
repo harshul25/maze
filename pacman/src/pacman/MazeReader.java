4178a584eb9f07b3ae1c07ce50f89fb3b1dec575
Harshul Mehta <harshul.mehta863@gmail.com>
2020-06-16 15:25:11 -0400

    Hope this works
    //Now when you create the object you will automatically instantiate the array and then using your getter method you will hopefully get the array created.

--- pacman/src/pacman/MazeReader.java
+++ pacman/src/pacman/MazeReader.java
@@ -42,13 +42,46 @@ public class MazeReader {
             System.out.println(Arrays.toString(arr[i]));
 
     }
+    
+    public MazeRunner(){
+    // create a constructor for the specific object 
+        
+        //read file into 2D array for position x,y
+        File file = new File("src/pacman/bigMaze.txt");
+
+        BufferedReader a = new BufferedReader(new FileReader(file));
+
+        String l;
+        int x_index = 0;
+        int start_x = 0;
+        int start_y = 0;
+
+        while ((l = a.readLine()) != null) {
+            //position for 2d array
+            for (int i = 0; i < l.length(); i++) {
+
+                if(l.charAt(i) == '%'){
+                    arr[x_index][i] = '0';}
+                else if(l.charAt(i) == 'P') {arr[x_index][i] = 'X';}
+                else if(l.charAt(i) == '.'){ arr[x_index][i] = 'S'; start_x = x_index; start_y= i;}
+                else {
+                    arr[x_index][i] = '1';
+                }
+            }
+            x_index ++;
+
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(Arrays.toString(arr[i]));
+    
+    }
 
     public static char[][] getArr() {
 
-        return arr;
+        return this.arr;
     }
 }
 
 
-
+ 
 
